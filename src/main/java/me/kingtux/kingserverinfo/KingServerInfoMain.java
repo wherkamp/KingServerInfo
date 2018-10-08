717ddd67b7cb43e737fb0e970e701e768540dd0b
wherkamp <wherkamp@kingtux.me>
2018-10-08 14:26:49 -0400

    More work getting ready for release

--- src/main/java/me/kingtux/kingserverinfo/KingServerInfoMain.java
+++ src/main/java/me/kingtux/kingserverinfo/KingServerInfoMain.java
@@ -1,8 +1,6 @@
 package me.kingtux.kingserverinfo;
 
 
-import java.lang.reflect.Field;
-import java.util.logging.Level;
 import me.kingtux.kingserverinfo.commands.ServerInfoCommand;
 import me.kingtux.kingserverinfo.events.ClickEvent;
 import me.kingtux.kingserverinfo.events.JoinEvent;
@@ -15,76 +13,99 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Field;
+import java.util.logging.Level;
+
 public class KingServerInfoMain extends JavaPlugin {
 
-  private ConfigManager configManager;
-  private ConfigSettings configSettings;
-  private MediaGui mediaGui;
+    private ConfigManager configManager;
+    private ConfigSettings configSettings;
+    private MediaGui mediaGui;
 
-  public void onEnable() {
-    if (Bukkit.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
-      configManager = new ConfigManager(this);
-      configManager.setupConfig();
+    public void onEnable() {
+        if (Bukkit.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            configManager = new ConfigManager(this);
+            configManager.setupConfig();
 
-      getLogger().log(Level.INFO,
-          ChatColor.translateAlternateColorCodes('&',
-              configSettings.getPrefix() + " Successfully Got the Config and Settings"));
-      try {
-        final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
-        bukkitCommandMap.setAccessible(true);
-        CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
-        commandMap.register(configSettings.getServerInfoCommand(),
-            new ServerInfoCommand(configSettings.getServerInfoCommand(),
-                configSettings.getServerInfoDescription(), this));
+            getLogger().log(Level.INFO,
+                    ChatColor.translateAlternateColorCodes('&',
+                            configSettings.getPrefix() + " Successfully Got the Config and Settings"));
 
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      enableEvents();
-      mediaGui = new MediaGui(this);
+            enableEvents();
+            getCommmandMap().register(configSettings.getServerInfoCommand(),
+                    new ServerInfoCommand(configSettings.getServerInfoCommand(),
+                            configSettings.getServerInfoDescription(), this));
+            mediaGui = new MediaGui(this);
 //      Metrics metrics = new Metrics(this);
 
-    } else {
-      getLogger().log(Level.SEVERE, "I find your lack of the PlaceHolderAPI disturbing.");
-      getServer().getPluginManager().disablePlugin(this);
+        } else {
+            getLogger().log(Level.SEVERE, "I find your lack of the PlaceHolderAPI disturbing.");
+            getServer().getPluginManager().disablePlugin(this);
+        }
+
+
+    }
+
+    public void reloadPlugin() {
+        configManager.reloadConfig();
+        configSettings.getAllSettings();
+
+        getLogger().log(Level.INFO,
+                ChatColor.translateAlternateColorCodes('&',
+                        configSettings.getPrefix() + " Successfully Got the Config and Settings"));
+
     }
 
 
-  }
+    public void onDisable() {
 
-  public void reloadPlugin() {
-    configManager.reloadConfig();
-    configSettings.getAllSettings();
+    }
 
-    getLogger().log(Level.INFO,
-        ChatColor.translateAlternateColorCodes('&',
-            configSettings.getPrefix() + " Successfully Got the Config and Settings"));
+    public ConfigSettings getConfigSettings() {
+        return configSettings;
+    }
 
-  }
+    public void setConfigSettings(ConfigSettings cF) {
+        this.configSettings = cF;
+    }
 
+    private void enableEvents() {
+        Bukkit.getServer().getPluginManager().registerEvents(new ClickEvent(this), this);
+        Bukkit.getServer().getPluginManager().registerEvents(new LeaveEvent(this), this);
+        Bukkit.getServer().getPluginManager().registerEvents(new JoinEvent(this), this);
 
-  public void onDisable() {
-    //Trying to clear out all used memory used by Configsettings
-    System.gc();
-  }
+    }
 
-  public ConfigSettings getConfigSettings() {
-    return configSettings;
-  }
+    private CommandMap getCommmandMap() {
+        if (isPaper()) {
+            getLogger().log(Level.INFO, "It's a Paper BOI");
+            try {
+                Bukkit.getServer().getCommandMap();
+                //Just in case
+            } catch (NoSuchMethodError e) {
+                getLogger().log(Level.INFO, "Paper's getCommandMap is missing reverting to old method.");
+            }
+        }
+        try {
+            final Field bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            bukkitCommandMap.setAccessible(true);
+            return (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-  public void setConfigSettings(ConfigSettings cF) {
-    this.configSettings = cF;
-  }
+    private boolean isPaper() {
+        try {
+            Class.forName("com.destroystokyo.paper.PaperConfig");
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
 
-  private void enableEvents() {
-    Bukkit.getServer().getPluginManager().registerEvents(new ClickEvent(this), this);
-    Bukkit.getServer().getPluginManager().registerEvents(new LeaveEvent(this), this);
-    Bukkit.getServer().getPluginManager().registerEvents(new JoinEvent(this), this);
-
-  }
-
-
-  public MediaGui getMediaGui() {
-    return mediaGui;
-  }
+    public MediaGui getMediaGui() {
+        return mediaGui;
+    }
 }
